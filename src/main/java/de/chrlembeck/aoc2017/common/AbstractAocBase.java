e6c10e25ea40eb739461f5eb3f0120448fb56915
Christoph Lembeck <lec@viadee.de>
2017-12-05 21:06:10 +0100

    Zeitmessung hinzugefügt

--- src/main/java/de/chrlembeck/aoc2017/common/AbstractAocBase.java
+++ src/main/java/de/chrlembeck/aoc2017/common/AbstractAocBase.java
@@ -1,6 +1,10 @@
 package de.chrlembeck.aoc2017.common;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public abstract class AbstractAocBase {
 
@@ -15,16 +19,26 @@ public abstract class AbstractAocBase {
     public abstract String getInputLocation(int part);
 
     public void run() {
+
         final String input1 = getInput1();
         System.out.println("Input 1: \n'" + input1 + "'\n");
         final String input2 = getInput2();
         System.out.println("Input 2: \n'" + input2 + "'\n");
         try (Scanner scanner = new Scanner(input1)) {
-            System.out.println("Part 1: " + part1(scanner));
+            printResult(scanner, "Part 1: ", this::part1);
         }
         try (Scanner scanner = new Scanner(input2)) {
-            System.out.println("Part 2: " + part2(scanner));
+            printResult(scanner, "Part 2: ", this::part2);
         }
+    }
+
+    private void printResult(final Scanner scanner, final String prefix, final Function<Scanner, String> function) {
+        final LocalTime start = LocalTime.now();
+        System.out.println(prefix + function.apply(scanner));
+        final LocalTime end = LocalTime.now();
+        final Duration duration = Duration.between(start, end);
+        System.out.println(LocalTime.MIDNIGHT.plus(duration).format(
+                DateTimeFormatter.ofPattern("HH 'hours', mm ' minutes', ss 'seconds', AAA 'milliseconds'")));
     }
 
     public String getInput1() {
