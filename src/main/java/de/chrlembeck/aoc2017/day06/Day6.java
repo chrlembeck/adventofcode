ed1e847c3e35d3c4268f8b228a807f88f385f5bd
Christoph Lembeck <lec@viadee.de>
2017-12-06 21:45:10 +0100

    day6

---
+++ src/main/java/de/chrlembeck/aoc2017/day06/Day6.java
@@ -0,0 +1,78 @@
+package de.chrlembeck.aoc2017.day06;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+import de.chrlembeck.aoc2017.common.AbstractAocBase;
+
+public class Day6 extends AbstractAocBase {
+
+    public static void main(final String[] args) {
+        new Day6().run();
+    }
+
+    @Override
+    public String part1(final Scanner input) {
+        final Set<List<Integer>> history = new HashSet<>();
+        List<Integer> bank = loadBank(input);
+        int count = 0;
+        while (history.add(bank)) {
+            count++;
+            bank = allocate(bank);
+        }
+
+        return Integer.toString(count);
+    }
+
+    @Override
+    public String part2(final Scanner input) {
+        List<Integer> bank = loadBank(input);
+        final Map<List<Integer>, Integer> history = new HashMap<>();
+        Integer firstOccurence;
+        int count = 0;
+        while ((firstOccurence = history.put(bank, count)) == null) {
+            count++;
+            bank = allocate(bank);
+        }
+        return Integer.toString(count - firstOccurence);
+    }
+
+    private List<Integer> loadBank(final Scanner input) {
+        final List<Integer> bank = new ArrayList<>();
+        while (input.hasNextInt()) {
+            final int value = input.nextInt();
+            bank.add(value);
+        }
+        return bank;
+    }
+
+    private List<Integer> allocate(final List<Integer> bank) {
+        int max = 0;
+        int maxIdx = 0;
+        for (int i = 0; i < bank.size(); i++) {
+            final int value = bank.get(i);
+            if (value > max) {
+                max = value;
+                maxIdx = i;
+            }
+        }
+        final List<Integer> newBank = new ArrayList<>(bank);
+        newBank.set(maxIdx, 0);
+        for (int i = 1; i <= max; i++) {
+            final int idx = (maxIdx + i) % newBank.size();
+            newBank.set(idx, newBank.get(idx) + 1);
+        }
+
+        return newBank;
+    }
+
+    @Override
+    public String getInputLocation(final int part) {
+        return "/input/day06.txt";
+    }
+}
---
+++ src/test/java/de/chrlembeck/aoc2017/day06/Day6Test.java
@@ -0,0 +1,37 @@
+package de.chrlembeck.aoc2017.day06;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class Day6Test {
+
+    static String test(final String input, final Function<Scanner, String> function) {
+        try (Scanner scanner = new Scanner(input)) {
+            return function.apply(scanner);
+        }
+    }
+
+    @Test
+    @Ignore
+    public void test1() {
+        final Day6 day6 = new Day6();
+        assertEquals("5", test("0 2 7 0", day6::part1));
+        assertEquals("4074", test(day6.getInput1(), day6::part1));
+    }
+
+    @Test
+    @Ignore
+    public void test2() {
+        final Day6 day6 = new Day6();
+        assertEquals("4", test("0  2   7   0", day6::part2));
+        assertEquals("2793", test(day6.getInput2(), day6::part2));
+    }
+}
