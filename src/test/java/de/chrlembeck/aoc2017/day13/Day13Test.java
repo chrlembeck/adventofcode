5dd6c941c738a3237da5e353aa4a7823b0c50bd4
Christoph Lembeck <lec@viadee.de>
2017-12-13 20:54:57 +0100

    day 13

--- src/test/java/de/chrlembeck/aoc2017/day13/Day13Test.java
+++ src/test/java/de/chrlembeck/aoc2017/day13/Day13Test.java
@@ -21,14 +21,20 @@ public class Day13Test {
     @Test
     public void test1() {
         final Day13 day13 = new Day13();
-        assertEquals(null, test("", day13::part1));
-        assertEquals(null, test(day13.getInput1(), day13::part1));
+        assertEquals("24", test("0: 3\n" +
+                "1: 2\n" +
+                "4: 4\n" +
+                "6: 4", day13::part1));
+        assertEquals("1476", test(day13.getInput1(), day13::part1));
     }
 
     @Test
     public void test2() {
         final Day13 day13 = new Day13();
-        assertEquals(null, test("", day13::part2));
-        assertEquals(null, test(day13.getInput2(), day13::part2));
+        assertEquals("10", test("0: 3\n" +
+                "1: 2\n" +
+                "4: 4\n" +
+                "6: 4", day13::part2));
+        assertEquals("3937334", test(day13.getInput2(), day13::part2));
     }
 }
