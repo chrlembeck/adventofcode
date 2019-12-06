37785e382b835abdaf72492cf15e465d7bac93cd
Christoph Lembeck <chrlembeck@gmail.com>
2019-12-06 09:08:00 +0100

    more tests

--- src/test/java/de/chrlembeck/aoc2015/Aoc2015Day19Test.java
+++ src/test/java/de/chrlembeck/aoc2015/Aoc2015Day19Test.java
@@ -11,6 +11,8 @@ public class Aoc2015Day19Test {
     @Test
     public void test1() {
         final Aoc2015Day19 day = new Aoc2015Day19();
+        assertEquals("4", test("H => HO\nH => OH\nO => HH\n\nHOH", day::part1));
+        assertEquals("7", test("H => HO\nH => OH\nO => HH\n\nHOHOHO", day::part1));
         assertEquals("509", test(day.getInput1(), day::part1));
     }
 
