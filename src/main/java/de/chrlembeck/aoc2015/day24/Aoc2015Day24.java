cedf813ea1258e73f35bea09fe9a728a46c42769
Christoph Lembeck <chrlembeck@gmail.com>
2021-12-16 10:14:45 +0100

    speedup part 1

--- src/main/java/de/chrlembeck/aoc2015/day24/Aoc2015Day24.java
+++ src/main/java/de/chrlembeck/aoc2015/day24/Aoc2015Day24.java
@@ -2,7 +2,7 @@ package de.chrlembeck.aoc2015.day24;
 
 import de.chrlembeck.aoccommon.AbstractAocBase;
 import java.util.*;
-import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Aoc2015Day24 extends AbstractAocBase {
@@ -21,29 +21,42 @@ public class Aoc2015Day24 extends AbstractAocBase {
         return result;
     }
 
-    public static final long quantumEntanglement(List<Integer> list) {
-        return list.stream().mapToLong(Integer::longValue).reduce(1, (a,b)->a*b);
+    @Override
+    public String part2(final Scanner input) {
+        return "";
+    }
+
+    public static final long quantumEntanglement(final Integer[] list) {
+        long result = list[0];
+        for (int i = list.length - 1; i > 0; i--) {
+            result *= list[i];
+        }
+        return result;
     }
 
     private long findSolutions(List<Integer> weights, int groupWeight) {
         LengthSortedResult pairs = findGroup1(weights, groupWeight);
-        for (Pair pair: pairs) {
-            LengthSortedResult p2s = findGroup1(pair.list2, groupWeight);
+        for (Integer[] pair : pairs) {
+            List<Integer> other = new ArrayList<>(weights);
+            for (Integer i: pair) {
+                other.remove(i);
+            }
+            LengthSortedResult p2s = findGroup1(other, groupWeight);
             if (p2s.size() > 0) {
-                return quantumEntanglement(pair.list1);
+                return quantumEntanglement(pair);
             }
         }
         return -1;
     }
 
     private static LengthSortedResult findGroup1(List<Integer> weights, int groupWeight) {
-        LengthSortedResult res = new LengthSortedResult(weights.size()+1);
-        BiConsumer<List<Integer>, List<Integer>> collector = (l1, l2) -> res.add(new Pair(new ArrayList<>(l1), new ArrayList<>(l2)));
-        findGroup1(weights, groupWeight, new ArrayList<>(), collector, 0, new ArrayList<>());
+        LengthSortedResult res = new LengthSortedResult(weights.size() + 1);
+        Consumer<Integer[]> collector = l1 -> res.add(l1);
+        findGroup1(weights, groupWeight, new ArrayList<>(), collector, 0);
         return res;
     }
 
-    private static void findGroup1(List<Integer> weights, int remaining, List<Integer> result, BiConsumer<List<Integer>, List<Integer>> resultConsumer, int startAt, List<Integer> unused) {
+    private static void findGroup1(List<Integer> weights, int remaining, List<Integer> result, Consumer<Integer[]> resultConsumer, int startAt) {
         for (int i = startAt; i < weights.size(); i++) {
             Integer current = weights.get(i);
             if (current > remaining) {
@@ -51,26 +64,15 @@ public class Aoc2015Day24 extends AbstractAocBase {
             }
             result.add(current);
             if (remaining == current) {
-                List<Integer> test = new ArrayList<>(unused.size() + weights.size() - i + 1); // check length
-                test.addAll(unused);
-                test.addAll(weights.subList(i + 1, weights.size()));
-                resultConsumer.accept(result, test);
+                resultConsumer.accept(result.toArray(Integer[]::new));
             } else if (remaining > current) {
-                List<Integer> unusedCopy = new ArrayList<>(weights.size());
-                unusedCopy.addAll(unused);
-                findGroup1(weights, remaining - current, result, resultConsumer, i + 1, unusedCopy);
+                findGroup1(weights, remaining - current, result, resultConsumer, i + 1);
             }
             Integer removed = result.remove(result.size() - 1);
             if (!(removed.intValue() == current.intValue())) {
                 throw new IllegalStateException();
             }
-            unused.add(current);
         }
-    }
-
-    @Override
-    public String part2(final Scanner input) {
-        return "";
     }
 
     @Override
@@ -78,24 +80,9 @@ public class Aoc2015Day24 extends AbstractAocBase {
         return "/input/aoc2015/aoc2015day24.txt";
     }
 
-    static class Pair {
-        private List<Integer> list1;
-        private List<Integer> list2;
+    static class LengthSortedResult implements Iterable<Integer[]> {
 
-        public Pair(List<Integer> list1, List<Integer> list2) {
-            this.list1 = list1;
-            this.list2 = list2;
-        }
-
-        @Override
-        public String toString() {
-            return list1 + " -> "+ list2;
-        }
-    }
-
-    static class LengthSortedResult implements Iterable<Pair>{
-
-        private final List<Pair>[] data;
+        private final List<Integer[]>[] data;
 
         private int size;
 
@@ -107,13 +94,13 @@ public class Aoc2015Day24 extends AbstractAocBase {
 
         }
 
-        public void add(Pair pair) {
-            data[pair.list1.size()].add(pair);
+        public void add(Integer[] pair) {
+            data[pair.length].add(pair);
             size++;
         }
 
         @Override
-        public Iterator<Pair> iterator() {
+        public Iterator<Integer[]> iterator() {
             return Arrays.stream(data).flatMap(l -> l.stream()).iterator();
         }
 
